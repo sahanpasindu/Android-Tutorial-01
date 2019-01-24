41466dab02631b428b262eebe70166b12f4089be
Sahan Pasindu Nirmal <sahanpasindu07@gmail.com>
2019-01-24 11:23:53 +0530

    first commit

---
+++ app/src/main/java/ccsl/com/android_tutorial_01/MainActivity.java
@@ -0,0 +1,13 @@
+package ccsl.com.android_tutorial_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
